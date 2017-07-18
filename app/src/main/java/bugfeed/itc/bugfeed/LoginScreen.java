49254bdb6c28c16c0963079d85a389378e2c1823
deepakj619 <jaindeepak619@gmail.com>
2017-07-18 19:09:07 +0530

    Update LoginScreen.java

--- app/src/main/java/bugfeed/itc/bugfeed/LoginScreen.java
+++ app/src/main/java/bugfeed/itc/bugfeed/LoginScreen.java
@@ -9,6 +9,6 @@ public class LoginScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
-        System.out.println("hello");
+        System.out.println("hi");
     }
 }
