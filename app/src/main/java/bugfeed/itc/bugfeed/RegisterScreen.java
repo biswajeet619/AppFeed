f0772e55177149c0b80d3a13d2496917e3d7ab59
root <jaindeepak619@gmail.com>
2017-07-18 19:41:55 +0530

    new

--- app/src/main/java/bugfeed/itc/bugfeed/RegisterScreen.java
+++ app/src/main/java/bugfeed/itc/bugfeed/RegisterScreen.java
@@ -3,11 +3,11 @@ package bugfeed.itc.bugfeed;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginScreen extends AppCompatActivity {
+public class RegisterScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_screen);
+        setContentView(R.layout.activity_register_screen);
     }
 }
